003c612d644ff04439119cebb3833e0593ea3170
Keegan B. Smith <SMITKB1@student.op.ac.nz>
2019-06-05 14:34:24 +1200

    Start first floor methods

--- game/game/Program.cs
+++ game/game/Program.cs
@@ -8,8 +8,109 @@ namespace game
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void lvl2()
         {
+            string decision;
+            int count = 0;
+
+            if (count <= 0)
+            {
+                Console.WriteLine("Level 2: First Floor");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("You've made your way to the kitchen. On the bench you see a knife.\nTo the west is a doorway that leads to the living room.\nTo the east is a strange door that emits a faint glow.");
+            }
+            else
+            {
+                Console.WriteLine("You're back in the kitchen. To the west is a doorway that leads to the living room.\nTo the east is a strange door that emits a faint glow.");
+            }
+            
+            decision = Console.ReadLine();
+
+            switch(decision)
+            {
+                case "east":
+                    strangeDoor();
+                    count++;
+                    break;
+                case "west":
+                    livingRoom();
+                    count++;
+                    break;
+                case "knife":
+                    break;
+                    
+                    
+            }
+        }
+
+        public static void strangeDoor()
+        {
+            string decision; 
+            Console.WriteLine("You walk up to the strange glowing door. Beside the door is a button.");
+            decision = Console.ReadLine();
+            switch (decision)
+            {
+                case "button":
+                    eaButton();
+                    break;
+
+                    
+
+
+            }
+
+            
+        }
+
+        public static void eaButton()
+        {
+            Console.WriteLine("You press the button and a deep voice booms from the door.");
+            Console.WriteLine("\"EA has placed this door here to impede your progress. Pay $50 to pass!\"");
+            Console.ReadLine();
+            //if ()
+            //{
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("");
+            //}
+        }
+
+        public static void livingRoom()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main()
+        {
+            lvl2();
         }
     }
 }
