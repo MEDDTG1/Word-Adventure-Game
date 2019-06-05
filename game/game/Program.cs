ec922098fc22c5a423727c917328b93d8f9003cf
Sophie Morrison (1000041080) <MORRSR3@student.op.ac.nz>
2019-06-05 14:25:14 +1200

    added foundations for level 3

--- game/game/Program.cs
+++ game/game/Program.cs
@@ -8,8 +8,61 @@ namespace game
 {
     class Program
     {
+        public static void Upstairs()
+        {
+            int choice;
+            Console.WriteLine("Level 3: Upstairs");
+            Console.WriteLine("You arrive at the top of the stairs. There is a door to your left with a mysterious substance " +
+                "dripping from the handle, a door straight ahead with a strange sound coming from within and a door to your " +
+                "right with a white 'X' painted on it. There is also a ladder which appears to lead to the roof.");
+            Console.WriteLine();
+            Console.WriteLine("1 - Left Door");
+            Console.WriteLine("2 - Middle Door");
+            Console.WriteLine("3 - Right Door");
+            Console.WriteLine("4 - Ladder");
+            Console.Write("Where are you going?: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+            }
+
+        }
+
+        public static void leftDoor()
+        {
+
+        }
+
+        public static void middleDoor()
+        {
+
+        }
+
+        public static void rightDoor()
+        {
+
+        }
+
+        public static void ladder()
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            Upstairs();
         }
     }
 }
