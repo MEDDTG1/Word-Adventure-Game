e04bedd323c650dd1b25f32caa6fc1b1b01b3367
Taylor Medder (1000068269) <MEDDTG1@student.op.ac.nz>
2019-06-05 14:35:32 +1200

    Taylors changes

--- game/game/Program.cs
+++ game/game/Program.cs
@@ -2,14 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace game
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void level1()
         {
+            int temp = 0;
+            Console.WriteLine("You wake up on the cold hard floor of a dusty pitch black room \nnot sure where you are or how you got here.");
+            Thread.Sleep(200);
+            Console.WriteLine("For now your main objective should be to escape this mysterious house \nit seems there is a staircase leading upto a door\n");
+            Thread.Sleep(200);
+            Console.WriteLine(" Check the Door: 1       Look Elsewhere: 2");
+            temp = Convert.ToInt32(Console.ReadLine());
+            if (temp == 1)
+            {
+                level1_1a();
+            }
+            else
+            {
+                level1_1b();
+            }
+        }
+        public static void level1_1a()
+        {
+            int temp = 0;
+            Console.WriteLine("You check the door to see that it has a sign sayging 'Pay $14.99 to unlock' What do you do");
+            Console.WriteLine(" Pay the 15 dollars: 1     Turn Around: 2");
+            temp = Convert.ToInt32(Console.ReadLine());
+            if (temp == 1)
+            {
+                EA_Ending();
+            }
+            else
+            {
+                level1_1b();
+            }
+
+        }
+        public static void level1_1b()
+        {
+            Console.WriteLine("You fell down the stairs and died");
+        }
+
+        public static void EA_Ending()
+        {
+            Console.WriteLine("You open the door to a nice view of the bois in their car ready to hitup the KFC and scrrt down to the matukituki");
+        }
+        public static void menu()
+        {
+            Console.WriteLine("     Do you want to play     ");
+            Console.WriteLine("");
+            Console.WriteLine("    Play: 1         Exit: 0  ");
+            int play;
+            play = Convert.ToInt32(Console.ReadLine());
+            if (play == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            else if (play > 1)
+            {
+                Console.WriteLine("Invalid Input");
+                Console.ReadLine();
+                Main();
+            }
+            else
+            {
+                level1();
+            }
+        }
+        static void Main()
+        {
+            menu();
+        //level1();
+        //level2();
+        //level3();
+        //level4();
         }
     }
 }
